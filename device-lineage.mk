95d85002678af134c23296f12a0c78b733b34f7c
Pranav Vashi <neobuddy89@gmail.com>
2024-11-03 21:04:52 +0530

    caimito: Build PixelParts if available
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device-lineage.mk
+++ device-lineage.mk
@@ -28,6 +28,9 @@ PRODUCT_PACKAGES += \
 # PowerShare
 include hardware/google/pixel/powershare/device.mk
 
+# Pixel Parts
+$(call inherit-product-if-exists, packages/apps/PixelParts/device.mk)
+
 # Satellite
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/conf/allowlist_satellite.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/allowlist_satellite.xml
