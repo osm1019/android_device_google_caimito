28eff0c84d15b48a3fb1cb79e1fb580701df3c55
Pranav Vashi <neobuddy89@gmail.com>
2024-12-01 14:18:24 +0530

    caimito: Include google facelock when available
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device-lineage.mk
+++ device-lineage.mk
@@ -21,6 +21,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay
 
+# Face unlock
+$(call inherit-product-if-exists, vendor/google/faceunlock/config.mk)
+
+# Fingerprint
+TARGET_HAS_UDFPS := true
+
 # IWLAN
 PRODUCT_PACKAGES += \
     Iwlan
